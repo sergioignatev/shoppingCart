cb5afa439324005cf40959916890232c13ae3aa3
sergioIgnatev <sergioignatev17@gmail.com>
2022-07-20 16:10:21 +0300

    add delete buttons

---
+++ scripts.js
@@ -0,0 +1,34 @@
+function myF(){
+    let inp=document.getElementById("input").value;
+    document.getElementById('input').value = ''
+    document.getElementById("demo").innerHTML +=`<li class="mt-3" ondblclick="d(this)" >${inp} <button class="btn btn-danger" onclick="complete(this)" >Куплено?</button></li>`}
+    
+    function d(x){
+    var display=""
+    if(confirm(`Удалить?`)){
+    display="none"
+    }else{
+    display=""
+    }
+    
+    
+    x.style.display=display;
+    x.style.height="10px"
+    }
+    function complete(x){
+      x.innerHTML="✓ Куплено"
+      x.classList.add("btn-success")
+      x.classList.remove("btn-danger")
+    }
+    function rem(){
+        let completed=document.getElementsByClassName("btn-success")
+        for(let i=0;i<completed.length;i++){
+            completed[i].parentElement.style.display="none"
+        }
+    }
+    function deleteAll(){
+        if(confirm('Удалить все?')){
+            document.getElementById("demo").innerHTML=""
+        }
+        else{}
+    }
